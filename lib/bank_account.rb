c35f41bb57e9f6bd3efbf1baa72ce68a43b19ba2
Lisa <bardelli.lisa@gmail.com>
2021-02-08 18:45:52 +0000

    Run rubocop

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -32,10 +32,10 @@ class BankAccount
     @transactions.each do |transaction|
       temp_balance += transaction.cash_amount
       if transaction.cash_amount.positive?
-        row = "#{transaction.date}|| #{'%.2f' % transaction.cash_amount.to_f} || || #{'%.2f' % temp_balance}"
+        row = "#{transaction.date}|| #{format('%.2f', transaction.cash_amount)} || || #{format('%.2f', temp_balance)}"
       else
 
-        row = "#{transaction.date} || || #{format('%.2f', (-1 * transaction.cash_amount.to_f))} || #{'%.2f' % temp_balance}"
+        row = "#{transaction.date} || || #{format('%.2f', (-1 * transaction.cash_amount))} || #{format('%.2f', temp_balance)}"
       end
       p row.center(60)
     end
